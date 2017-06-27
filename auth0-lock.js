dc25c9ecdbbe62597fb99137451ff52329274e8a
qftgtr <qft.gtr@gmail.com>
2017-06-28 01:07:16 +0800

    Update auth0-lock.js

--- auth0-lock.js
+++ auth0-lock.js
@@ -1,30 +1,32 @@
-var { NativeModules, Platform } = require('react-native');
-var LockModule = NativeModules.Auth0LockModule;
+const { NativeModules, Platform } = require('react-native');
+
+const LockModule = NativeModules.Auth0LockModule;
 
 const VERSION = require('./version');
-const Auth0 = require('react-native-auth0');
+
+import Auth0 from 'react-native-auth0';
 
 class Auth0Lock {
   constructor(options) {
-    let { clientId, domain, style } = options;
+    const { clientId, domain, style } = options;
     if (options != null && clientId != null && domain != null) {
       this.lockOptions = {
-        clientId: clientId,
-        domain: domain,
-        style: style,
+        clientId,
+        domain,
+        style,
         configurationDomain: options.configurationDomain,
         libraryVersion: VERSION,
-        useBrowser: options.useBrowser
+        useBrowser: options.useBrowser,
       };
       this.nativeIntegrations = options.integrations;
-      this.auth0 = new Auth0(domain);
+      this.auth0 = new Auth0({ domain, clientId });
     } else {
-      throw "Must supply clientId & domain";
+      throw 'Must supply clientId & domain';
     }
   }
 
   hide(callback) {
-    if (Platform.OS === "android") {
+    if (Platform.OS === 'android') {
       setTimeout(() => callback(), 0);
       return;
     }
@@ -33,15 +35,15 @@ class Auth0Lock {
 
   show(options, callback) {
     LockModule.init(this.lockOptions);
-    if (Platform.OS === "ios" && this.nativeIntegrations) {
+    if (Platform.OS === 'ios' && this.nativeIntegrations) {
       LockModule.nativeIntegrations(this.nativeIntegrations);
     }
     LockModule.show(options, callback);
   }
 
   authenticate(connectionName, options, callback) {
-    if (Platform.OS === "android") {
-      callback("Not available in Android", null, null);
+    if (Platform.OS === 'android') {
+      callback('Not available in Android', null, null);
       return;
     }
     LockModule.init(this.lockOptions);
@@ -52,7 +54,22 @@ class Auth0Lock {
   }
 
   authenticationAPI() {
-    return this.auth0.authentication(this.lockOptions.clientId);
+    return this.auth0.auth;
+  }
+
+  refreshToken(token) {
+    return fetch(`${this.auth0.auth.client.baseUrl}/delegation`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        grant_type: 'urn:ietf:params:oauth:grant-type:jwt-bearer',
+        client_id: this.auth0.auth.clientId,
+        refresh_token: token,
+        api_type: 'app',
+      }),
+    }).then(response => response.json());
   }
 
   usersAPI(token) {
